48fedf8028c50e96badd226a570679821685df07
Zach Leatherman <zachleatherman@gmail.com>
2020-06-13 15:28:40 -0500

    Try again

--- plugins/keep-data-cache/index.js
+++ plugins/keep-data-cache/index.js
@@ -4,12 +4,12 @@ module.exports = {
   //  - the file/directory already exists locally
   //  - the file/directory has not been cached yet
   async onPreBuild({ utils }) {
-    await utils.cache.restore('./_data/results/');
-  }
+    await utils.cache.restore('./_data/results');
+  },
   // Cache file/directory for future builds.
   // Does not do anything if:
   //  - the file/directory does not exist locally
   async onPostBuild({ utils }) {
-    await utils.cache.save('./_data/results/');
+    await utils.cache.save('./_data/results');
   }
 };
