219800e8fc0f0ac8a779e0ab8e7d7ee54cc2c473
Zach Leatherman <zachleatherman@gmail.com>
2020-06-14 14:00:12 -0500

    Only run the tests at most once per hour

--- plugins/keep-data-cache/index.js
+++ plugins/keep-data-cache/index.js
@@ -4,12 +4,14 @@ module.exports = {
   //  - the file/directory already exists locally
   //  - the file/directory has not been cached yet
   async onPreBuild({ utils }) {
+    await utils.cache.restore('./_data/results-last-run.json');
     await utils.cache.restore('./_data/results');
   },
   // Cache file/directory for future builds.
   // Does not do anything if:
   //  - the file/directory does not exist locally
   async onPostBuild({ utils }) {
+    await utils.cache.save('./_data/results-last-run.json');
     await utils.cache.save('./_data/results');
   }
 };
--- run-tests.js
+++ run-tests.js
@@ -5,18 +5,26 @@ const PerfLeaderboard = require("performance-leaderboard");
 const NUMBER_OF_RUNS = 3;
 
 (async function() {
-	let urls = require("./test-sites.js");
+	let today = Date.now();
+	let dataDir = `./_data/`;
+	let lastRunFilename = `${dataDir}results-last-run.json`;
+	const lastRun = require(lastRunFilename);
+	if(today - lastRun.timestamp < 1000*60*60) {
+		console.log( "Test ran less than an hour ago, just building the site." );
+		return;
+	}
+	await fs.writeFile(lastRunFilename, JSON.stringify({ timestamp: today }, null, 2));
 
+	let urls = require("./test-sites.js");
 	let results = await PerfLeaderboard(urls, NUMBER_OF_RUNS, {
 		freshChrome: "site"
 	});
 
-	let today = Date.now();
 
 	let promises = [];
 	for(let result of results) {
 		let id = shortHash(result.url);
-		let dir = `./_data/results/${id}/`;
+		let dir = `${dataDir}results/${id}/`;
 		let filename = `${dir}date-${today}.json`;
 		await fs.mkdir(dir, { recursive: true });
 		promises.push(fs.writeFile(filename, JSON.stringify(result, null, 2)));
