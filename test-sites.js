ab9545db623bb4e75ea2f2dbb17457342e0733b0
Zach Leatherman <zachleatherman@gmail.com>
2020-06-14 14:49:28 -0500

    Add jamstackconf site

--- test-sites.js
+++ test-sites.js
@@ -2,5 +2,7 @@ module.exports = [
 	"https://www.netlify.com/",
 	"https://www.netlify.com/donation-matching/",
 	"https://www.netlify.com/products/build/plugins/",
-	"https://www.netlify.com/with/sitecore/"
+	"https://www.netlify.com/with/sitecore/",
+	"https://jamstackconf.com/",
+	"https://jamstackconf.com/virtual/",
 ]
