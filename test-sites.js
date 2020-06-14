21b7cd08bae59604a2aa7a35c4cbd68781e0fc9c
Zach Leatherman <zachleatherman@gmail.com>
2020-06-13 20:23:39 -0500

    Reinstate for all sites.

--- test-sites.js
+++ test-sites.js
@@ -1,6 +1,6 @@
 module.exports = [
 	"https://www.netlify.com/",
-	// "https://www.netlify.com/donation-matching/",
-	// "https://www.netlify.com/products/build/plugins/",
-	// "https://www.netlify.com/with/sitecore/"
+	"https://www.netlify.com/donation-matching/",
+	"https://www.netlify.com/products/build/plugins/",
+	"https://www.netlify.com/with/sitecore/"
 ]
