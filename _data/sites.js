228c9b823b4fb77169dc3b23a7042197c5170d6e
Zach Leatherman <zachleatherman@gmail.com>
2020-06-16 16:50:00 -0500

    Adds drupal and wordpress pages

--- _data/sites.js
+++ _data/sites.js
@@ -9,9 +9,11 @@ module.exports = {
 			"https://www.netlify.com/donation-matching/",
 			"https://www.netlify.com/products/build/plugins/",
 			"https://www.netlify.com/with/sitecore/",
+			"https://www.netlify.com/with/drupal/",
+			"https://www.netlify.com/with/wordpress/",
+			"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
 			"https://jamstackconf.com/",
 			"https://jamstackconf.com/virtual/",
-			"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
 		]
 	},
 	ssg: {
