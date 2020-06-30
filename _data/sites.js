b25cdf61c1ed638d5a5bc367c5166e9a4275cb6f
Nicolas Hoizey <nicolas@hoizey.com>
2020-06-30 22:41:10 +0200

    Configure sites

--- _data/sites.js
+++ _data/sites.js
@@ -1,35 +1,45 @@
 module.exports = {
-	netlify: {
-		description: "Netlify web sites",
-		options: {
-			freshChrome: "site",
-		},
-		urls: [
-			"https://www.netlify.com/",
-			"https://www.netlify.com/donation-matching/",
-			"https://www.netlify.com/products/build/plugins/",
-			"https://www.netlify.com/with/sitecore/",
-			"https://www.netlify.com/with/drupal/",
-			"https://www.netlify.com/with/wordpress/",
-			"https://www.netlify.com/webinar/a-drupal-journey-to-the-jamstack/",
-			// "https://www.netlify.com/pricing/", // Having Lighthouse issues with LCP on this page
-			"https://jamstackconf.com/",
-			"https://jamstackconf.com/virtual/",
-		]
-	},
-	ssg: {
-		description: "Static Site Generator web sites",
-		options: {
-			freshChrome: "site"
-		},
-		urls: [
-			"https://www.11ty.dev/",
-			"https://www.gatsbyjs.org/",
-			"https://gohugo.io/",
-			"https://nextjs.org/",
-			"https://nuxtjs.org/",
-			// "https://vuepress.vuejs.org/", // kept timing out in lighthouse
-			"https://gridsome.org/",
-		]
-	}
+  nicolashoizeycom: {
+    description: "nicolas-hoizey.com",
+    options: {
+      freshChrome: "site",
+    },
+    urls: [
+      "https://nicolas-hoizey.com/",
+      "https://nicolas-hoizey.com/articles/",
+      "https://nicolas-hoizey.com/articles/2018/08/01/using-cloudinary-s-fetch-api-to-convert-an-animated-gif-to-a-video/",
+      "https://nicolas-hoizey.com/links/",
+      "https://nicolas-hoizey.com/links/2020/01/29/humans-can-t-read-urls-how-can-we-fix-it/",
+      "https://nicolas-hoizey.com/notes/",
+      "https://nicolas-hoizey.com/talks/",
+    ],
+  },
+  pack11ty: {
+    description: "pack11ty.dev",
+    options: {
+      freshChrome: "site",
+    },
+    urls: ["https://pack11ty.dev/"],
+  },
+  esviji: {
+    description: "esviji.com",
+    options: {
+      freshChrome: "site",
+    },
+    urls: ["https://esviji.com/", "https://play.esviji.com/"],
+  },
+  nhoio: {
+    description: "nho.io",
+    options: {
+      freshChrome: "site",
+    },
+    urls: ["https://nho.io/"],
+  },
+  preciousprana: {
+    description: "precious-prana.com",
+    options: {
+      freshChrome: "site",
+    },
+    urls: ["https://precious-prana.com/"],
+  },
 };
