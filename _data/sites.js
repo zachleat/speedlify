fc7772f3bbf804aedf6fbb8142ad57b9a68504d0
Zach Leatherman <zachleatherman@gmail.com>
2020-07-01 15:33:13 -0500

    Adds asymmetric max update frequencies

--- _data/sites.js
+++ _data/sites.js
@@ -2,6 +2,7 @@ module.exports = {
 	netlify: {
 		description: "Netlify web sites",
 		options: {
+			frequency: 60 * 2, // 2 hours
 			freshChrome: "site",
 		},
 		urls: [
@@ -19,6 +20,7 @@ module.exports = {
 	ssg: {
 		description: "Static Site Generator web sites",
 		options: {
+			frequency: 60 * 24, // 24 hours
 			// if the sites don’t share assets on the same domain, we can reset
 			// chrome with each run instead of each site in every run (it’s faster)
 			freshChrome: "run"
