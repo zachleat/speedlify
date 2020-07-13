a6ce5b9f28aa93c01cfcd921be13374aa9095f1e
Max Böck <hello@mxb.at>
2020-07-13 15:51:57 +0200

    replace demo URLs with my own

---
+++ _data/sites/mxb.js
@@ -0,0 +1,19 @@
+module.exports = {
+    name: 'mxb.dev',
+    description: 'Personal website',
+    options: {
+        frequency: 60 * 23, // 23 hours
+        freshChrome: 'site'
+    },
+    urls: [
+        'https://mxb.dev/',
+        'https://mxb.dev/blog/',
+        'https://mxb.dev/notes/',
+        'https://mxb.dev/about/',
+        // Popular Posts
+        'https://mxb.dev/blog/the-return-of-the-90s-web/',
+        'https://mxb.dev/blog/color-theme-switcher/',
+        'https://mxb.dev/blog/using-webmentions-on-static-sites/',
+        'https://mxb.dev/blog/emergency-website-kit/'
+    ]
+}
