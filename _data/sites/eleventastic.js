fee94b43a7b2a17abc0597330e22c210f85689cc
Max Böck <hello@mxb.at>
2020-07-17 19:35:48 +0200

    add eleventastic

---
+++ _data/sites/eleventastic.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'eleventastic',
+    description: 'Eleventy Starter Kit',
+    options: {
+        frequency: 60 * 23, // 23 hours
+        freshChrome: 'site'
+    },
+    urls: ['https://eleventastic.netlify.app/']
+}
