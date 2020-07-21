3589b68453369125fbc5b16a9b41b14096f0f62c
Zach Leatherman <zachleatherman@gmail.com>
2020-07-21 15:06:50 -0500

    Re-establish frequency

--- _data/sites/netlify.js
+++ _data/sites/netlify.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: "Netlify", // optional, falls back to object key
 	description: "Netlify web sites",
 	options: {
-		frequency: 60 * 11 + 30, // 11h, 30m
+		frequency: 60 * 0 + 30, // 11h, 30m
 		// Use "run" if the sites don’t share assets on the same origin
 		//           and we can reset chrome with each run instead of
 		//           each site in every run (it’s faster)
