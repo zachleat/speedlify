3e550a3ab2ba81a9fdd405dfc1f29cd7ea250784
Zach Leatherman <zachleatherman@gmail.com>
2020-08-04 22:12:40 -0500

    Use hours instead of days

--- assets/timestamp-ago.js
+++ assets/timestamp-ago.js
@@ -9,9 +9,9 @@
 			let timestamp = this.getAttribute("timestamp");
 			if(timestamp) {
 				let date = (new Date()).setTime(timestamp);
-				let diff = ((date - Date.now())/(1000 * 60 * 60 * 24)).toFixed(1);
+				let diff = Math.floor((date - Date.now())/(1000 * 60 * 60));
 				this.setAttribute("title", this.innerText);
-				this.innerText = rtf.format(diff, "day");
+				this.innerText = rtf.format(diff, "hour");
 			}
 		}
 	});
