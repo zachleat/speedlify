ebfd5d598e2606494d5a6178e4a558ff98c7a593
Zach Leatherman <zachleatherman@gmail.com>
2020-08-21 20:08:22 -0500

    Move into regular results

--- _data/sites/built-with-eleventy.js
+++ _data/sites/built-with-eleventy.js
@@ -10,36 +10,17 @@ module.exports = async function() {
 	return {
 		name: "built-with-eleventy", // optional, falls back to object key
 		description: "Web Sites Built with Eleventy",
+
 		// don’t run in Production or on Branch/Deploy previews
 		// this one is localhost only.
 		skip: process.env.CONTEXT,
 
 		// Hide from category list when hosted
-		hide: process.env.CONTEXT && process.env.SITE_NAME !== "speedlify",
+		hide: false,
 
 		options: {
 			// Don’t show speedlify score or rank for sites with more than 2 or more yellow circles
 			noShame: true,
-
-			// You probably won’t need this, unless the same URL lives in multiple
-			// verticals/categories share the same URLs.
-
-			// Eventually this will be the default but if you turn it on for a category
-			// with existing data on Netlify cache, Speedlify won’t be able to find your
-			// results!
-			isolated: true,
-
-			// Data directory, results go into `_data/manual` instead of `_data/results`
-			useManualResultsDir: true,
-
-			runs: 3,
-			frequency: 60 * 24 * 1 - 30, // 1 day - 30 minutes
-
-			// Use "run" if the sites don’t share assets on the same origin
-			//           and we can reset chrome with each run instead of
-			//           each site in every run (it’s faster)
-			// Use "site" if sites are all on the same origin and share assets.
-			freshChrome: "run"
 		},
 		urls: urlsJson
 	};
