6586b911ccafe4f4d1e0ed80443525af1b6b7bc1
Zach Leatherman <zachleatherman@gmail.com>
2020-09-06 14:08:20 -0500

    Per-group estimated time per build

--- run-tests.js
+++ run-tests.js
@@ -27,12 +27,12 @@ const prettyTime = (seconds) => {
 	);
 }
 
-async function tryToPreventNetlifyBuildTimeout(dateTestsStarted, numberOfUrls) {
-	let minutesRemaining = NETLIFY_MAX_LIMIT - (Date.now() - dateTestsStarted)/(1000*60)
+async function tryToPreventNetlifyBuildTimeout(dateTestsStarted, numberOfUrls, estimatedTimePerBuild = ESTIMATED_MAX_TIME_PER_TEST) {
+	let minutesRemaining = NETLIFY_MAX_LIMIT - (Date.now() - dateTestsStarted)/(1000*60);
 	if(process.env.CONTEXT &&
 		process.env.CONTEXT === "production" &&
 		NETLIFY_MAX_LIMIT &&
-		minutesRemaining < numberOfUrls * ESTIMATED_MAX_TIME_PER_TEST) {
+		minutesRemaining < numberOfUrls * estimatedTimePerBuild) {
 		console.log( `run-tests has about ${minutesRemaining} minutes left, but the next run has ${numberOfUrls} urls. Saving it for the next build.` );
 		return true;
 	}
@@ -76,7 +76,7 @@ async function tryToPreventNetlifyBuildTimeout(dateTestsStarted, numberOfUrls) {
 			continue;
 		}
 
-		if(await tryToPreventNetlifyBuildTimeout(dateTestsStarted, group.urls.length)) {
+		if(await tryToPreventNetlifyBuildTimeout(dateTestsStarted, group.urls.length, group.estimatedTimePerBuild)) {
 			// stop everything, we’re too close to the timeout
 			return;
 		}
