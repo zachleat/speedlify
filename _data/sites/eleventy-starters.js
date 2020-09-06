53d532f94a34b8c52ec5e02e41c0a306ccd61dde
Zach Leatherman <zachleatherman@gmail.com>
2020-09-06 14:33:34 -0500

    Still a bit too many sites

--- _data/sites/eleventy-starters.js
+++ _data/sites/eleventy-starters.js
@@ -13,7 +13,7 @@ module.exports = async function() {
 		// skip if localhost
 		// skip if this is a new fork of the speedlify (not Zach’s)
 		skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
-		estimatedTimePerBuild: .5,
+		estimatedTimePerBuild: .3,
 		options: {
 			frequency: 60 * 23, // 23 hours
 			// Use "run" if the sites don’t share assets on the same origin
