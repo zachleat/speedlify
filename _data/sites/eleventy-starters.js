7a4b3706868b769706c0f6c101c5ba1e79af95ac
Zach Leatherman <zachleatherman@gmail.com>
2020-12-14 16:11:47 -0600

    Not sure the best way to do this but we have too many starters to run in one build so I need something to get the builds running again.

--- _data/sites/eleventy-starters.js
+++ _data/sites/eleventy-starters.js
@@ -1,5 +1,14 @@
 const CacheAsset = require("@11ty/eleventy-cache-assets");
 
+function randomizeArray(arr) {
+	let a = arr.slice(0);
+	for (let i = a.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[a[i], a[j]] = [a[j], a[i]];
+	}
+	return a;
+}
+
 module.exports = async function() {
 	let url = "https://www.11ty.dev/api/starters.json";
 	let urlsJson = await CacheAsset(url, {
@@ -15,7 +24,7 @@ module.exports = async function() {
 		skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 		estimatedTimePerBuild: .2,
 		options: {
-			frequency: 60 * 23, // 23 hours
+			frequency: 60 * 11.5, // 11.5 hours
 			// Use "run" if the sites don’t share assets on the same origin
 			//           and we can reset chrome with each run instead of
 			//           each site in every run (it’s faster)
@@ -23,6 +32,6 @@ module.exports = async function() {
 			freshChrome: "run"
 		},
 
-		urls: urlsJson
+		urls: randomizeArray(urlsJson).slice(0,40) // a random sample of 40
 	};
 };
