f505bded37a7e4bf23e823f5cf089780f4f2e9ba
Max Böck <hello@mxb.at>
2021-01-05 12:10:39 +0100

    skip post URLs

--- _data/sites/mxb.js
+++ _data/sites/mxb.js
@@ -9,12 +9,7 @@ module.exports = {
         'https://mxb.dev/',
         'https://mxb.dev/blog/',
         'https://mxb.dev/notes/',
-        'https://mxb.dev/about/',
-        // Popular Posts
-        'https://mxb.dev/blog/the-return-of-the-90s-web/',
-        'https://mxb.dev/blog/color-theme-switcher/',
-        'https://mxb.dev/blog/using-webmentions-on-static-sites/',
-        'https://mxb.dev/blog/emergency-website-kit/'
+        'https://mxb.dev/about/'
     ],
     skip: false
 }
