7bd9bd514c47e8675d266da35601f24779905f09
Zach Leatherman <zachleatherman@gmail.com>
2021-04-15 20:37:57 -0500

    Cherry pick updated icons

---
+++ _data/lighthouseMaximums.js
@@ -0,0 +1,20 @@
+module.exports = {
+  average: {
+    fcp: 4000,
+    si: 5800,
+    lcp: 4000,
+    tti: 7300,
+    tbt: 600,
+    cls: 0.25,
+  },
+  good: {
+    fcp: 2336,
+    si: 3387,
+    lcp: 2500,
+    tti: 3875,
+    tbt: 287,
+    cls: 0.1,
+    // fid: 130,
+  },
+  
+};
