21dba2613f69abc6dbd90ad5f0a534fe6a3db57f
Zach Leatherman <zachleatherman@gmail.com>
2021-12-14 09:51:30 -0600

    Adds default chrome flags `--disable-dev-shm-usage` to the group options. See https://github.com/zachleat/speedlify/issues/44

--- run-tests.js
+++ run-tests.js
@@ -106,10 +106,14 @@ async function tryToPreventNetlifyBuildTimeout(dateTestsStarted, numberOfUrls, e
 
 		let runCount =
 			group.options && group.options.runs ? group.options.runs : NUMBER_OF_RUNS;
+		let options = Object.assign({
+			chromeFlags: ['--headless', '--disable-dev-shm-usage']
+		}, group.options);
+
 		let results = await PerfLeaderboard(
 			group.urls,
 			runCount,
-			group.options || {}
+			options,
 		);
 
 		let promises = [];
