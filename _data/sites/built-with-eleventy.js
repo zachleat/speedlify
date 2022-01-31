dba477a28f72f5baf0a2ea1b0ee818add00935ce
Zach Leatherman <zachleatherman@gmail.com>
2022-01-30 21:22:29 -0600

    Update deps

--- _data/sites/built-with-eleventy.js
+++ _data/sites/built-with-eleventy.js
@@ -3,7 +3,7 @@ const CacheAsset = require("@11ty/eleventy-cache-assets");
 module.exports = async function() {
 	let url = "https://www.11ty.dev/api/urls.json";
 	let urlsJson = await CacheAsset(url, {
-		duration: "1h",
+		duration: "2d",
 		type: "json",
 	});
 
@@ -37,8 +37,6 @@ module.exports = async function() {
 
 		// this was a bug when two different URLs resolved to the same URL and conflicted in resolvedUrl results.
 		// 1-indexed
-		skipIndeces: [
-			207
-		]
+		skipIndeces: []
 	};
 };
