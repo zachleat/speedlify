8b2d0003ae5ce3b1be2250354202a66abeb24868
gamoridev <gamoridev@protonmail.com>
2022-02-22 10:49:19 -0300

    feat: map emcasa pages to speedlify runner

---
+++ _data/sites/emcasa.js
@@ -0,0 +1,16 @@
+module.exports = {
+  name: "emcasa-home", // optional, falls back to object key
+  description: "Páginas emcasa.com",
+  options: {
+    frequency: 60 * 23, // (in minutes), 23 hours
+    freshChrome: "site", // Use "site" if sites are all on the same origin and share assets.
+  },
+  urls: [
+    "https://www.emcasa.com",
+    "https://www.emcasa.com/imoveis/sp/sao-paulo",
+    "https://www.emcasa.com/imoveis/sp/sao-paulo/perdizes/rua-diana/id-1451",
+    "https://www.emcasa.com/especialistas-de-vendas",
+    "https://www.emcasa.com/vender",
+    "https://www.emcasa.com/corretor-parceiro",
+  ],
+};
