8447ca4b50d7681f05c23c508ca2b69eae2c325b
Zach Leatherman <zachleatherman@gmail.com>
2023-02-13 10:39:07 -0600

    Eleventy starters as a separate branch

--- _data/sites/eleventy-starters.js
+++ _data/sites/eleventy-starters.js
@@ -11,9 +11,6 @@ module.exports = async function() {
 		name: "eleventy-starters", // optional, falls back to object key
 		description: "Eleventy Starter Projects",
 		unordered: true,
-		// skip if localhost
-		// skip if this is a new fork of the speedlify (not Zach’s)
-		skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
 		options: {
 			package: "performance-leaderboard-pagespeed-insights",
 			frequency: 60 * 23, // 23 hours
