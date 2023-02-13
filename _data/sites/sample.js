64386a48a98e655dc6f1f117a299f7f5c352daaa
Zach Leatherman <zachleatherman@gmail.com>
2023-02-13 12:00:26 -0600

    Swap to run sample every time

--- _data/sites/sample.js
+++ _data/sites/sample.js
@@ -2,7 +2,8 @@ module.exports = {
 	name: "Sample", // optional, falls back to object key
 	description: "The default sites that get tested",
 	options: {
-		frequency: 60 * 23, // (in minutes), 23 hours
+		runs: 1,
+		frequency: 1, // (in minutes)
 	},
 	urls: [
 		"https://www.speedlify.dev/"
