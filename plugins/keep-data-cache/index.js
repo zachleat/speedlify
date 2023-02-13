c43d0446e3d49e0ddc136eec5c431c70b2b8e936
Zach Leatherman <zachleatherman@gmail.com>
2023-02-13 12:26:51 -0600

    Another approach

--- plugins/keep-data-cache/index.js
+++ plugins/keep-data-cache/index.js
@@ -4,6 +4,8 @@ module.exports = {
   //  - the file/directory already exists locally
   //  - the file/directory has not been cached yet
   async onPreBuild({ utils }) {
+    await utils.cache.restore('/opt/buildhome/.cache/'); // for Netlify Puppeteer
+
     await utils.cache.restore('./_data/results-last-runs.json');
     await utils.cache.restore('./_data/results');
   },
@@ -11,6 +13,8 @@ module.exports = {
   // Does not do anything if:
   //  - the file/directory does not exist locally
   async onPostBuild({ utils }) {
+    await utils.cache.save('/opt/buildhome/.cache/'); // for Netlify Puppeteer
+
     await utils.cache.save('./_data/results-last-runs.json');
     await utils.cache.save('./_data/results');
   }
